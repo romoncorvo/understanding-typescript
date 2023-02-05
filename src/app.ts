c1439be75661910b7414902735845a77c905f8f5
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-02-05 19:38:21 +0100

    Callbacks

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,14 @@ function printResult(num: number) {
   console.log(`Result: ${num}`);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(1, 2));
 
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
+addAndHandle(8, 2, printResult);
+addAndHandle(1, 20, (result) => {
+  console.log(result + 5);
+});
