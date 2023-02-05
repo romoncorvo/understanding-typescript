d2db719799feae759c0e95df66d38d0404df6e58
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-02-05 18:06:30 +0100

    Enum

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Rodrigo',
+//   age: 29,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+
+const person = {
   name: 'Rodrigo',
   age: 29,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.AUTHOR
 };
 
-console.log(person.name);
+console.log(person);
 
 person.hobbies.forEach((hobby) => {
   console.log(hobby.toUpperCase());
