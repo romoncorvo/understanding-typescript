eed5e53f5a00b6a01a0db36110ac0ad342c0d584
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-02-05 15:29:15 +0100

    Number and boolean

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Time to get started...');
+function add(n1: number, n2: number, showResult: boolean) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }else {
+        return n1 + n2;
+    }
+} 
+
+const number1 = 5;
+const number2 = 10;
+const printResult = true;
+
+const result = add(number1, number2, printResult)
+console.log(result);
