158bfdd4d1d93611667b78e242aa6317925a0e80
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-02-05 16:24:27 +0100

    Add eslint

--- app.ts
+++ app.ts
@@ -1,14 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    if (showResult) {
-        console.log(phrase + (n1 + n2));
-    }else {
-        return n1 + n2;
-    }
-} 
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Rodrigo',
+  age: 29,
+};
 
-const number1 = 5;
-const number2 = 10;
-const printResult = true;
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase)
+console.log(person.name);
