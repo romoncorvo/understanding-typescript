c166b9e9f4d8e87d8581d574f52baa32808ce39d
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-02-06 22:25:26 +0100

    Function binding

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ const button = document.querySelector('button');
 function clickHandler(message: string) {
   console.log(`clicked ${message}`);
 }
+
 if (button) {
   button.addEventListener('click', clickHandler.bind(null, 'the button'));
 }
