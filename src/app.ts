b1b52bce79a5fdd70998156103b2df83cac17754
Rodrigo Moncorvo <romoncorvo@gmail.com>
2023-02-11 13:29:11 +0100

    Another generic function

--- src/app.ts
+++ src/app.ts
@@ -18,3 +18,21 @@ function merge<T extends Object, U extends Object>(objA: T, objB: U) {
 const mergedObj = merge({ name: 'Rodrigo' }, { age: 29 });
 
 console.log(mergedObj.name);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements.`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe([2, 5, 4]));
+console.log(countAndDescribe([]));
